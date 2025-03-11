0784937efeb497dee66f893449906f52ab62313c
Raghad-Alahmadi <tu4152758@taibahu.edu.sa>
2025-03-11 16:41:41 +0300

    Implmented the User API

---
+++ task-manager-backend/service.UserAPI/Data/AppDbContext.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using service.UserAPI.Models;
+
+namespace service.UserAPI.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<User> Users { get; set; }
+    }
+}
---
+++ task-manager-backend/service.UserAPI/Models/User.cs
@@ -0,0 +1,9 @@
+﻿namespace service.UserAPI.Models
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public required string Username { get; set; }
+        public required string Email { get; set; }
+    }
+}
