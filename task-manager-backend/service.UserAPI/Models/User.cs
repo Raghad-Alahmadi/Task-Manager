50c6daa737bc719f28bd907ce1eeb83e4b3599d0
Raghad-Alahmadi <145593793+Raghad-Alahmadi@users.noreply.github.com>
2025-03-12 14:11:46 +0300

    Added the Annotation to the User model

--- task-manager-backend/service.UserAPI/Models/User.cs
+++ task-manager-backend/service.UserAPI/Models/User.cs
@@ -1,9 +1,18 @@
-﻿namespace service.UserAPI.Models
+namespace service.UserAPI.Models
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class User
     {
+        [Key]
         public int Id { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 3)]
         public required string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
         public required string Email { get; set; }
     }
 }
