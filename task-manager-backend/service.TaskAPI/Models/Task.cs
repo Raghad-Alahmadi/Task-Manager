5ca2b25227d4fa12e08841690ddb93d862682cd4
Raghad-Alahmadi <145593793+Raghad-Alahmadi@users.noreply.github.com>
2025-03-12 14:12:53 +0300

    Added the Annotation to the Task model

--- task-manager-backend/service.TaskAPI/Models/Task.cs
+++ task-manager-backend/service.TaskAPI/Models/Task.cs
@@ -1,10 +1,19 @@
 ﻿namespace service.TaskAPI.Models
 {
+    using System.ComponentModel.DataAnnotations;
+
     public class Task
     {
+        [Key]
         public int Id { get; set; }
-        public string Title { get; set; }
-        public string Description { get; set; }
+
+        [Required]
+        [StringLength(100)]
+        public string Title { get; set; } = string.Empty;
+
+        [StringLength(500)]
+        public string Description { get; set; } = string.Empty;
+
         public bool Completed { get; set; }
     }
 }
